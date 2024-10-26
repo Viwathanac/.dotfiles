3df43a9852d5b49797d4860cb2e17a1be867f60e
Viwathanac Sothun <viwathanacsothun@gmail.com>
2024-10-25 23:26:55 -0400

    move .config into nvim/

--- .config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- All the configs are in lua/kiwi/
-require("kiwi")
-
--- Starts lazy.nvim in kiwi/config/lazy.lua
-require("kiwi.config.lazy")
--- .config/nvim/lua/kiwi/plugins/harpoon.lua
+++
@@ -1,5 +0,0 @@
-local M = {
-	"ThePrimeagen/harpoon"
-}
-
-return M
