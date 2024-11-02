82980618af6d2954f12c0a8cc7db097df314c3b6
Viwathanac Sothun <viwathanacsothun@gmail.com>
2024-11-02 07:51:33 -0400

    added vim settings and keybinds

--- nvim/.config/nvim/lua/kiwi/init.lua
+++ nvim/.config/nvim/lua/kiwi/init.lua
@@ -1,16 +1,4 @@
 -- <<Remapping Keybinds automatically>>
 require("kiwi.remap")
-
--- <<Showing Relative Line Number for Every Nvim Start>>
-vim.cmd("set nu rnu")
-vim.cmd("set cursorline")
-
--- <<Stop Highlighting After ENTER/RETURN>> 
-vim.cmd("nnoremap <CR> :noh<CR><CR>")
-
--- <<Centers Cursor>>
-vim.cmd("nnoremap <C-d> <C-d>zz")
-vim.cmd("nnoremap <C-u> <C-u>zz")
-vim.cmd("nnoremap n nzzzv")
-vim.cmd("nnoremap N Nzzzv")
+require("kiwi.settings") -- Vim Settings
 
--- nvim/.config/nvim/lua/kiwi/remap.lua
+++ nvim/.config/nvim/lua/kiwi/remap.lua
@@ -1,7 +1,24 @@
--- <<General>> : General Settings
-vim.cmd("set shiftwidth=4")
-vim.cmd("set tabstop=4")
-
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<CR>", ":noh<CR><CR>", {})
+
+vim.keymap.set("n", "Q", "<Nop>", { desc = "Remove annoying exmode" })
+vim.keymap.set("n", "q:", "<Nop>", { desc = "Remove annoying exmode" })
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("i", ",", ",<c-g>u")
+vim.keymap.set("i", ".", ".<c-g>u")
+vim.keymap.set("i", "!", "!<c-g>u")
+vim.keymap.set("i", "?", "?<c-g>u")
+
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { silent = true })
+
+vim.keymap.set("v", "<", "<gv", { desc = "Dedent current selection" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent current selection" })
 
