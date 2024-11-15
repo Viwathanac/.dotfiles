8d6cfd3b3a8dafabaf36718260614a826df2cad2
Viwathanac Sothun <viwathanacsothun@gmail.com>
2024-11-15 17:02:58 -0500

    i3 now works with 2 monitor

--- bin/.local/scripts/launch_polybar.sh
+++ bin/.local/scripts/launch_polybar.sh
@@ -2,20 +2,20 @@
 killall -q polybar
 
 # <<Launching Polybar>>
-echo "---" | tee -a /tmp/polybar1.log 
-polybar toph 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars Launched"
+#echo "---" | tee -a /tmp/polybar1.log 
+#polybar toph 2>&1 | tee -a /tmp/polybar1.log & disown
+#
+#echo "Bars Launched"
 
 # This should be the script used when Monitor and Laptop 
 # isn't Mirrored
-# :%s/##//g to remove the comments
+# :%s///g to remove the comments
 
-##if type "xrandr" ; then
-	##for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		##MONITOR=$m polybar --reload toph &
-	##done
-##else
-	##polybar --reload toph &
-##fi
-##
+if type "xrandr" ; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload toph &
+	done
+else
+	polybar --reload toph &
+fi
+
