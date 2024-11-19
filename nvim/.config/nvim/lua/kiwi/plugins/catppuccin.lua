88168655f86d5ec8a9d2a2a34e7a1d2ed182e052
Viwathanac <viwathanacsothun@gmail.com>
2024-11-19 18:06:55 -0500

    changed color theme to catppuccin

--- nvim/.config/nvim/lua/kiwi/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/kiwi/plugins/catppuccin.lua
@@ -1,10 +1,10 @@
 local M = {
-	"folke/tokyonight.nvim",
+	"catppuccin/nvim",
 	lazy = false,
 	priority = 1000,
 	opts = {},
 	config = function()
-		vim.cmd.colorscheme("tokyonight-night")
+		vim.cmd.colorscheme("catppuccin")
 	end
 }
 return M
